07e875c323a1fb39e97ea14ec092f49135450e48
abdkhanstd <55620704+abdkhanstd@users.noreply.github.com>
2023-10-26 19:14:29 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='abdutils',
+    version='0.1',
+    description='A Python utility module for file and folder operations',
+    author='ABD',
+    author_email='abdkhan@163.com',
+    url='https://github.com/abdkhanstd/abdutils',
+    packages=['abdutils'],
+)
