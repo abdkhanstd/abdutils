260348c6e900bd4ba6ae5f1f2f62730d36d22dcc
abdkhanstd <55620704+abdkhanstd@users.noreply.github.com>
2023-10-27 12:02:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='abdutils',
     version='0.1',
     description='A Python utility module that enhances the ease and reliability of Python programming.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='ABD',
     author_email='abdkhan@163.com',
     url='https://github.com/abdkhanstd/abdutils',
-    packages=find_packages(),  # Automatically find all packages in the directory
-    py_modules=['abdutils'],  # Specify the main module (Python file) to be included
+    py_modules=['abdutil'],  # Specify the main module (Python file) to be included
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    install_requires=[
+        'Pillow==9.1.0',
+        'opencv-python==4.8.1.78',
+        'matplotlib==3.7.3',
+        'numpy==1.23.1',
+    ],
 )
